69b263f4c08339d18e3510a3d21515e15c2dd9d3
Cristiano <cristiano@live.jp>
2015-11-10 22:02:53 -0200

    pronto

--- Dinner.py
+++ Dinner.py
@@ -1,29 +1,41 @@
-__author__ = 'Claudio'
+__author__ = ['Claudio', 'Cristiano']
+#
+#   Sistemas Operacionais - Jantar dos Filósofos
+#   BCC 6S
+#
+#   Claudio Roberto Costa RA 527033
+#   Cristiano Vicente RA 443913
+#
 # -*- coding: utf-8 -*-
-import _thread
-import time, random
-import threading
 
-garfo = list()
-for i in range(5):
-   garfo.append(threading.Semaphore(1))
+import time, random, threading
+
 
 def filosofo(f):
-   f = int(f)
-   while True:
-      # garfo da esquerda
-      garfo[f].acquire()
-      # garfo da direita
-      garfo[(f + 1) % 5].acquire()
-      print("Filósofo %i comendo..." %f)
-      time.sleep(random.randint(1, 5))
-      garfo[f].release()
-      garfo[(f + 1) % 5].release()
-      print ("Filósofo %i pensando..." %f)
-      time.sleep(random.randint(1, 10))
+    f = int(f)
+    while True:
+        # garfo da esquerda
+        garfo[f].acquire()  # P
+        # garfo da direita
+        garfo[(f + 1) % n].acquire()  # P
+        print("Filósofo %i comendo... :)" % f)
+        time.sleep(random.randint(1, 5))
+        garfo[f].release()  # V
+        garfo[(f + 1) % n].release()  # V
+        print("Filósofo %i pensando... :|" % f)
+        time.sleep(random.randint(1, 10))
 
-for i in range(5):
-   print ("Filósofo", i)
-   _thread.start_new_thread(filosofo, tuple([i]))
 
-while 1: pass
+if __name__ == "__main__":
+
+    n = 5
+
+    garfo = [threading.Semaphore(1) for i in range(n)]
+    for i in range(n):
+        print("Filósofo", i, "morto de fome :x")
+        threading._start_new_thread(filosofo, tuple([i]))
+
+    try:
+        while True: time.sleep(0.1)
+    except (KeyboardInterrupt, SystemExit):
+        exit(0)
